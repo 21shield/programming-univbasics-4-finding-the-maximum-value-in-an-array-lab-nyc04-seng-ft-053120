a6c21b63d896fe94ce4f0139da2af62bb168f280
Netaly Ramirez <21netaly@gmail.com>
2020-06-05 15:38:33 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,15 @@
 def find_max_value(array)
   # Add your solution here
+  # compare each element to each other, and get the index at where the largst number is found
+  index = 1
+  larger_element = array[0]
+  while index <= array.length-1 do
+    if larger_element < array[index]
+      # puts larger_element
+      larger_element = array[index]
+      #  puts larger_element
+    end
+   index += 1
+  end
+ larger_element
 end
